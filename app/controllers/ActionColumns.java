7399500e638c49a1dfced826f6a564c6d69d4d02
magnet88jp <magnet88jp@gmail.com>
2011-12-18 17:59:40 +0900

    implement DataColumn view

--- app/controllers/ActionColumns.java
+++ app/controllers/ActionColumns.java
@@ -1,8 +1,31 @@
 package controllers;
 
+import java.util.*;
 import play.*;
 import play.mvc.*;
 
+import models.*;
+
 @With(Secure.class)
-public class ActionColumns extends CRUD {}
+public class ActionColumns extends CRUD {
+
+  public static void json(Long action_id) {
+
+    List<DataColumn> datas = new ArrayList();
+    if (action_id == null) {
+      datas = DataColumn.findAll();
+    } else {
+      Action action = Action.findById(action_id);
+      List<ActionColumn> actionColumns = ActionColumn.find("byActionId", action.id.toString() ).fetch();
+      for( ActionColumn actionColumn : actionColumns) {
+        DataColumn dataColumn = DataColumn.findById(Long.parseLong(actionColumn.columnId));
+        datas.add(dataColumn);
+      }
+    }
+
+    renderJSON(datas);
+  }
+
+
+}
 
--- app/controllers/Actions.java
+++ app/controllers/Actions.java
@@ -1,51 +1,55 @@
 package controllers;
 
 import java.util.*;
+import java.io.*;
 import play.*;
 import play.data.binding.*;
 import play.mvc.*;
 import play.db.Model;
 import play.exceptions.*;
 import play.i18n.*;
+//import org.openqa.selenium.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.apache.commons.io.FileUtils;
+
 
 import models.*;
 
 @With(Secure.class)
 public class Actions extends CRUD {
 
-  public static void save(String id) throws Exception {
-    ObjectType type = ObjectType.get(getControllerClass());
-    notFoundIfNull(type);
-    Model object = type.findById(id);
-    notFoundIfNull(object);
-    Binder.bind(object, "object", params.all());
-    validation.valid(object);
-    if (validation.hasErrors()) {
-      renderArgs.put("error", Messages.get("crud.hasErrors"));
-      try {
-        render(request.controller.replace(".", "/") + "/show.html", type, object);
-      } catch (TemplateNotFoundException e) {
-        render("CRUD/show.html", type, object);
-      }
-    }
-    object._save();
+  @After( only={"blank", "show"} )
+  static void set( String id ) {
+    Logger.info("debug set");
+  }
 
+  @After( only={"create", "save"} )
+  static void take( Long id ) {
+    String[] datas1 = params.getAll("object.url");
     // action and column 
-    List<ActionColumn> actionColumns = ActionColumn.find("byActionId", id).fetch();
+
+//    Action action = new Action("","","","","","",new Function(1,"",""));
+    Action action = null;
+//    Action action;
+    if ( id == null ) {
+      List<Action> actions = Action.find(
+        "select a1 from Action a1 " +
+        "where a1.id = (select max(a2.id) from Action a2)").fetch();
+      for( Action data : actions ) {
+        action = data;
+        id = action.id;
+      }
+    } else {
+      action = Action.findById(id);
+    }
+
+    List<ActionColumn> actionColumns = ActionColumn.find("byActionId", id.toString()).fetch();
     for( ActionColumn actionColumn : actionColumns ) {
       actionColumn._delete();
     }
-//    Logger.info("params.all=%s", params.all() );
-//    Logger.info("params.object.datatable.datacolumn=%s", params.get("object.datatable.datacolumn") );
-//    Logger.info("params.object.title=%s", params.get("object.title") );
-//    Logger.info("params.object.dataTables.id=%s", params.get("object.dataTables.id").toString() );
-//    Logger.info("params.object.dataTables=%s", params.get("object.dataTables") );
-//    Logger.info("params.object.actionModes.id=%s", params.get("object.actionModes[0].id") );
-    String[] columns = params.getAll("object.datatable.datacolumn");
-    Logger.info("size=%d", columns.length );
 
-//  
-    Action action = Action.findById(Long.parseLong(id));
+    String[] columns = params.getAll("object.datatable.datacolumn");
+
     int colCount = 0;
     for(DataTable datatable : action.dataTables) {
 //      Logger.info("count=%d", DataColumn.find("byDataTable", datatable).fetch().size() );
@@ -56,18 +60,80 @@ public class Actions extends CRUD {
       for(String col : columns) {
         Logger.info("col.val=%s", col);
         if(!col.isEmpty()){
-          ActionColumn data = new ActionColumn(id, col);
+          ActionColumn data = new ActionColumn(id.toString(), col);
           data._save();
         }
       }
     }
 
-    flash.success(Messages.get("crud.saved", type.modelName));
-    if (params.get("_save") != null) {
-      redirect(request.controller + ".list");
-    }
-    redirect(request.controller + ".show", object._key());
   }
+
+
+//  public static void save(String id) throws Exception {
+//    ObjectType type = ObjectType.get(getControllerClass());
+//    notFoundIfNull(type);
+//    Model object = type.findById(id);
+//    notFoundIfNull(object);
+//    Binder.bind(object, "object", params.all());
+//    validation.valid(object);
+//    if (validation.hasErrors()) {
+//      renderArgs.put("error", Messages.get("crud.hasErrors"));
+//      try {
+//        render(request.controller.replace(".", "/") + "/show.html", type, object);
+//      } catch (TemplateNotFoundException e) {
+//        render("CRUD/show.html", type, object);
+//      }
+//    }
+//    object._save();
+//
+//    // action and column 
+//    List<ActionColumn> actionColumns = ActionColumn.find("byActionId", id).fetch();
+//    for( ActionColumn actionColumn : actionColumns ) {
+//      actionColumn._delete();
+//    }
+////    Logger.info("params.all=%s", params.all() );
+////    Logger.info("params.object.datatable.datacolumn=%s", params.get("object.datatable.datacolumn") );
+////    Logger.info("params.object.title=%s", params.get("object.title") );
+////    Logger.info("params.object.dataTables.id=%s", params.get("object.dataTables.id").toString() );
+////    Logger.info("params.object.dataTables=%s", params.get("object.dataTables") );
+////    Logger.info("params.object.actionModes.id=%s", params.get("object.actionModes[0].id") );
+//    String[] columns = params.getAll("object.datatable.datacolumn");
+//    Logger.info("size=%d", columns.length );
+//
+////  
+//    Action action = Action.findById(Long.parseLong(id));
+//    int colCount = 0;
+//    for(DataTable datatable : action.dataTables) {
+////      Logger.info("count=%d", DataColumn.find("byDataTable", datatable).fetch().size() );
+//      colCount += DataColumn.find("byDataTable", datatable).fetch().size();
+//    }
+//
+//    if( colCount != columns.length-1) {
+//      for(String col : columns) {
+//        Logger.info("col.val=%s", col);
+//        if(!col.isEmpty()){
+//          ActionColumn data = new ActionColumn(id, col);
+//          data._save();
+//        }
+//      }
+//    }
+//
+//// capture test
+////    WebDriver driver = new FirefoxDriver();
+////    driver.get("http://www.google.com/");
+////    File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//    // Now you can do whatever you need to do with it, for example copy somewhere     
+////    FileUtils.copyFile(scrFile, new File("screenshot.png"));
+////    driver.quit();
+//
+//// capture test end
+//
+//    flash.success(Messages.get("crud.saved", type.modelName));
+//    if (params.get("_save") != null) {
+//      redirect(request.controller + ".list");
+//    }
+//    redirect(request.controller + ".show", object._key());
+//  }
 
 }
 
